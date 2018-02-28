2496c956de884a86ec03c1a814a0623c7797ec3c
schmidtp0740 <schmidtp0740@gmail.com>
2018-02-28 21:12:19 +0000

    added more to initEMR function

--- chaincode/emrcc.go
+++ chaincode/emrcc.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
+	"strconv"
 	"encoding/json"	
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
@@ -20,6 +21,14 @@ type emr struct {
 	temp		string `json:"temp"`
 }
 
+// Main
+//=====
+func main(){
+        if err := shim.Start(new(EMR)); err != nil {
+                fmt.Printf("Error starting EMR chaincode: %s", err)
+        }
+}
+
 
 // Invoke - Initializes chaincode
 //===============================
@@ -31,36 +40,90 @@ func (t *EMR) Init (stub shim.ChaincodeStubInterface) peer.Response {
 //=========================================
 func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("invoke is running " + function)
 
 	// New, Update
 	if function == "initEMR"{
-		return t.initEMR(stub, args)
-	} else if function == "updateEMR" {
-		//assume update even if function is nil
-		return = t.updateEMR(stub, args)
+		return t.initEMR(stub, args) //create a new EMR
+	} else if function == "updateHB" {
+		return t.updateHB(stub, args) // Change heartBeat of owner
 	{
 
 	// Return the result as success payload
+	fmt.Println("invoke did not find func: " + function) 
 	return shim.Error("Received unknown function invocation")
 
-}
-
-// Main
-//=====
-func main(){
-	if err := shim.Start(new(EMR)); err != nil {
-		fmt.Printf("Error starting EMR chaincode: %s", err)
-	}
 }
 
 // New
 //====
 func (t *Chaincode) initEMR(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	var err error
+
+	// 	0	1	2	3	4
+	// 	"001"	"John"	"Doe"	"87"	"70"
+	if len(args) != 5 {
+		return shim.Error("Incorret number of arguments. Expecting 5")
+	}
+
+	// === Input Sanitation ===
+	fmt.Println("- start init EMR")
+	if len(args[0]) <= 0 {
+		return shim.Error("1st arguement must be a non-empty string")
+	} else if len(args[0]) <= 0 {
+		return shim.Error("2nd arguement must be a non-empty string")
+	} else if len(args[0] <= 0 {
+		return shim.Error("3rd arguement must be an non-empty string")
+	} else if len(args[0] <= 0 {
+		return shim.Error("4th arguement must be a non-empty string")
+	} else if len(args[0] <= 0 {
+		return shim.Error("5th arguement must be a non-empty string")
+	}
+
+	userId, err := strconv.Atoi(args[0])
+	if err != nil {
+		return shim.Error("1st arguement must be a numeric string")
+	}
+	firstName := strings.ToLower(args[1])
+	lastName := strings.ToLower(args[2])
+	heartBeat, err := strconv.Atoi(args[3])
+	if err != nil {
+		return shim.Error("4th arguement must be a numeric string")
+	}
+	temp, err := strconv.Atoi(args[4])
+	if err != nil {
+		return shim.Error("4th arguement must be a numeric string")
+	}
+
+	// == Check if EMR already exists ===
+	emrAsBytes, err := stub.GetState(userId)
+	if err != nil {
+		return shim.Error("Failed to get EMR: " + err.Error())
+	} else if emrAsBytes != nil {
+		fmt.Println("This EMR already exists: " + userId)
+		return shim.Error("This EMR already exists: " + userId)
+	}
+
+	// == Create EMR object and marchal to JSON ==
+	objectType := "EMR"
+	emr := &emr{objectType, userId, firstName, lastName, heartBeat, temp}
+	emrJSONasBytes, err := json.Marshal(emr)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	
+	// == Save marble to state ==
+	err = stub.PutState(userId, emrJSONasBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+
 }
 
 
 // Update
 //=======
-func (t *Chaincode) updateEMR(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+func (t *Chaincode) updateHB(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 }
 
